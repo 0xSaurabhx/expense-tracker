2f96212410212124d87495d48711e14aee678a1b
Saurabh <61317144+html-css-js-art@users.noreply.github.com>
2023-06-28 16:50:08 +0530

    Create Fire.js

---
+++ src/config/Fire.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBhSA4eQ9hLuF0PqBklJ8N2OpdObEp0uzc",
+  authDomain: "expense-edge.firebaseapp.com",
+  databaseURL: "https://expense-edge-default-rtdb.firebaseio.com",
+  projectId: "expense-edge",
+  storageBucket: "expense-edge.appspot.com",
+  messagingSenderId: "683706714826",
+  appId: "1:683706714826:web:b9506633833bf1845bc233",
+  measurementId: "G-QJYTH92M0G"
+};
+
+const fire = firebase.initializeApp(config);
+export default fire
